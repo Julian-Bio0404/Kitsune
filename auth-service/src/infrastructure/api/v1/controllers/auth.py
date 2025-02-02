c24dbc561520d499684bee99675ba0a3322731ba
Julian-Bio0404 <juliancamilohermida@hotmail.com>
2025-02-01 23:02:00 -0500

    Add authentication endpoint - Add authentication endpoint - Reorder routes

--- auth-service/src/infrastructure/api/v1/controllers/auth.py
+++ auth-service/src/infrastructure/api/v1/controllers/auth.py
@@ -1,17 +1,41 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Header, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
+from src import dependencies
+from src.domain.entities import User
+from src.domain.interfaces import UserRepositoryInterface
+from src.infrastructure.api.v1.schemas import UserSchema
+from src.infrastructure.api.v1.utils.jwt import decode_token
 
 router = APIRouter()
 
 
 @router.get("/")
-def auth() -> JSONResponse:
+def auth(authorization: str = Header(...)) -> JSONResponse:
     """
-    Auth endpoint.
+    Authentication endpoint.
     Return the user data according to token.
     """
-    data = {"message": "Hello world!"}
+    if not authorization:
+        content = jsonable_encoder({"detail": "Authorization header missing"})
+        return JSONResponse(content=content, status_code=status.HTTP_401_UNAUTHORIZED)
+
+    token = authorization.split(" ")[1]
+    payload = decode_token(token=token)
+    user_id = payload["id"]
+
+    repository = dependencies.get(UserRepositoryInterface)
+    user: User = repository.get(User.id == user_id)
+
+    data = UserSchema(
+        id=user.id,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        email=user.email,
+        created=user.created,
+        updated=user.updated,
+    ).data
+
     content = jsonable_encoder(data)
     return JSONResponse(content=content, status_code=status.HTTP_200_OK)
--- auth-service/src/infrastructure/api/v1/router.py
+++ auth-service/src/infrastructure/api/v1/router.py
@@ -4,7 +4,10 @@ from src.infrastructure.api.v1.controllers import auth, signup, login
 
 
 v1_router = APIRouter()
+auth_router = APIRouter()
 
-v1_router.include_router(router=auth.router, prefix="/auth", tags=["auth"])
-v1_router.include_router(router=signup.router, prefix="/signup", tags=["signup"])
-v1_router.include_router(router=login.router, prefix="/login", tags=["signup"])
+auth_router.include_router(router=auth.router, prefix="", tags=["auth"])
+auth_router.include_router(router=signup.router, prefix="/signup", tags=["signup"])
+auth_router.include_router(router=login.router, prefix="/login", tags=["login"])
+
+v1_router.include_router(router=auth_router, prefix="/auth", tags=["auth"])
